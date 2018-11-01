a01980cec2f11a1e8f87895cbed9b2bb224c76d6
surya474 <sganta1@miraclesoftl.com>
2018-11-02 03:02:20 +0530

    Update src/app/apiconfig.ts

--- src/app/apiconfig.ts
+++ src/app/apiconfig.ts
@@ -1,4 +1,17 @@
 export const restApi='http://209.97.130.34/lectrefy-api-service/'
+
 export const campaignapis={
-     addcampaigns:restApi+'api/v1.0/campaigns'    
+     addcampaigns:restApi+'api/v1.0/campaigns',
+     getcampaigns:restApi+'api/v1.0/campaigns',
+     getsinglecampaign:restApi+'api/v1.0/campaigns/',
+     campaignfilter:restApi+'api/v1.0/campaigns/',
+     update:restApi+'api/v1.0/campaigns/'    
+}
+
+export const contentApis={
+        getall:restApi+'api/v1.0/contents',
+        add:restApi+'api/v1.0/contents',
+        getSingle:restApi+'api/v1.0/contents/',
+        filter:restApi+'contents/filter?description=',
+        update:restApi+'api/v1.0/contents/'
 }
